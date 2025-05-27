946a764f3a5e0f2c806cfa1d9657ff4d8f609d6e
catsnax <107341849+catsnax@users.noreply.github.com>
2025-05-27 10:35:44 +0800

    fix: layout and responsiveness of signup page

--- front-end/.next/server/webpack-runtime.js
+++ front-end/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("48ba7247a818523d")
+/******/ 		__webpack_require__.h = () => ("850de79d05d8e22d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -168,11 +168,6 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
